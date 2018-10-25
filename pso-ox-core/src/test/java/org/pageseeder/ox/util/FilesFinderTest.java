92e8fdc0d142d3da387e4f9f2dad8fcd079c5cc5
Carlos Cabral <carlos.samuel@gmail.com>
2018-10-26 10:14:34 +1100

    Change test package from utils to util in order to match the original classes package

--- pso-ox-core/src/test/java/org/pageseeder/ox/util/FilesFinderTest.java
+++ pso-ox-core/src/test/java/org/pageseeder/ox/util/FilesFinderTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2018 Allette systems pty. ltd.
  */
-package org.pageseeder.ox.utils;
+package org.pageseeder.ox.util;
 
 import java.io.File;
 import java.util.List;
--- pso-ox-core/src/test/java/org/pageseeder/ox/util/PeriodicCleanerTest.java
+++ pso-ox-core/src/test/java/org/pageseeder/ox/util/PeriodicCleanerTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1999-2016 Allette systems pty. ltd.
  */
-package org.pageseeder.ox.utils;
+package org.pageseeder.ox.util;
 
 import java.io.File;
 import java.nio.file.Files;
