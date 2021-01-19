365259b6abc579aed0cb2d3e8ecd8ee0ca96f123
ccabral <ccabral@allette.com.au>
2021-01-19 13:40:11 +1100

    add logic for dynamic parameter (request and step parameters)

--- pso-ox-core/src/main/java/org/pageseeder/ox/util/StepUtils.java
+++ pso-ox-core/src/main/java/org/pageseeder/ox/util/StepUtils.java
@@ -2,8 +2,10 @@ package org.pageseeder.ox.util;
 
 import org.pageseeder.ox.api.StepInfo;
 import org.pageseeder.ox.core.PackageData;
+import org.pageseeder.ox.parameters.ParameterTemplate;
 
 import java.io.File;
+import java.util.Map;
 
 /**
  * The type Step utils.
@@ -60,6 +62,13 @@ public class StepUtils {
    * Get the parameter from step definition, if it is not found then gets from the request parameter.
    * Otherwise returns the fallback.
    *
+   * This method also implements dynamic parameter.
+   * It means that a parameter can have a value like "starting-text{extra-text}ending-text". If the extra-text is a
+   * existing parameter in the PackageData or StepInfo, then the {extra-text} will be replaced by its value.
+   * If extra-text does not exist, then it will be replaced by an empty value.
+   *
+   * In addition, the dynamic parameter can have a dafault value {extra-text=default extra text}
+   *
    * @param data PackageData
    * @param info StepInfo
    * @param parameterName The name of the parameter to get from step info or package data
@@ -67,9 +76,21 @@ public class StepUtils {
    * @return
    */
   public static String getParameter(PackageData data, StepInfo info, String parameterName, String fallback) {
-    String parameter = info.getParameter(parameterName, data.getParameter(parameterName));
+    String parameterValue = info.getParameter(parameterName, data.getParameter(parameterName));
+    if (StringUtils.isBlank(parameterValue)) {
+      parameterValue = fallback;
+    }
 
-    return !StringUtils.isBlank(parameter) ? parameter : fallback;
+    //Add request parameters
+    Map<String, String> parameters = data.getParameters();
+    //Add step parameters
+    parameters.putAll(info.parameters());
+
+    ParameterTemplate parameterTemplate = ParameterTemplate.parse(parameterValue);
+    parameterValue = parameterTemplate.toString(parameters);
+
+
+    return parameterValue;
   }
 
   /**
---
+++ pso-ox-core/src/test/java/org/pageseeder/ox/util/StepUtilsTest.java
@@ -0,0 +1,106 @@
+package org.pageseeder.ox.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.pageseeder.ox.api.StepInfo;
+import org.pageseeder.ox.core.PackageData;
+import org.pageseeder.ox.core.StepInfoImpl;
+import org.pageseeder.ox.parameters.ParameterTemplate;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ccabral
+ * @since 19 January 2021
+ */
+public class StepUtilsTest {
+
+  @Test
+  public void getParameter_LiteralValueFromPackageData_Success(){
+    Map<String, String> requestParameters = new HashMap<>();
+    requestParameters.put("test","Data test value");
+    PackageData data = createPackageData(requestParameters);
+    StepInfo info = createStepInfo(new HashMap<>());
+    Assert.assertEquals("Data test value", StepUtils.getParameter(data, info,"test", ""));
+  }
+
+  @Test
+  public void getParameter_LiteralValueFromStepInfo_Success(){
+    Map<String, String> stepParameters = new HashMap<>();
+    stepParameters.put("test","Info test value");
+    PackageData data = createPackageData(new HashMap<>());
+    StepInfo info = createStepInfo(stepParameters);
+    Assert.assertEquals("Info test value", StepUtils.getParameter(data, info,"test", ""));
+  }
+
+  @Test
+  public void getParameter_LiteralValueFromBoth_Success(){
+    Map<String, String> requestParameters = new HashMap<>();
+    requestParameters.put("test-data","Data test value");
+    PackageData data = createPackageData(requestParameters);
+    Map<String, String> stepParameters = new HashMap<>();
+    stepParameters.put("test-info","Info test value");
+    StepInfo info = createStepInfo(stepParameters);
+    Assert.assertEquals("Data test value", StepUtils.getParameter(data, info,"test-data", ""));
+    Assert.assertEquals("Info test value", StepUtils.getParameter(data, info,"test-info", ""));
+  }
+
+  @Test
+  public void getParameter_DynamicValueFromBoth_Success(){
+    Map<String, String> requestParameters = new HashMap<>();
+    requestParameters.put("test-data","{extra-info} data test value");
+    requestParameters.put("extra","extra text");
+    PackageData data = createPackageData(requestParameters);
+    Map<String, String> stepParameters = new HashMap<>();
+    stepParameters.put("test-info","Info test value {extra}");
+    stepParameters.put("extra-info","extra info text");
+    StepInfo info = createStepInfo(stepParameters);
+    Assert.assertEquals("extra info text data test value", StepUtils.getParameter(data, info,"test-data", ""));
+    Assert.assertEquals("Info test value extra text", StepUtils.getParameter(data, info,"test-info", ""));
+  }
+
+  @Test
+  public void getParameter_NonExistingDynamicValueFromBoth_Success(){
+    Map<String, String> requestParameters = new HashMap<>();
+    PackageData data = createPackageData(requestParameters);
+    Map<String, String> stepParameters = new HashMap<>();
+    stepParameters.put("test-info-non-exist","Info test value {extra-non-exist}");
+    StepInfo info = createStepInfo(stepParameters);
+    Assert.assertEquals("Info test value ", StepUtils.getParameter(data, info,"test-info-non-exist", ""));
+  }
+
+  @Test
+  public void getParameter_DefaultDynamicValueFromBoth_Success(){
+    Map<String, String> requestParameters = new HashMap<>();
+    PackageData data = createPackageData(requestParameters);
+    Map<String, String> stepParameters = new HashMap<>();
+    stepParameters.put("test-info-non-exist","Info test value {extra-non-exist=default extra text}");
+    StepInfo info = createStepInfo(stepParameters);
+    Assert.assertEquals("Info test value default extra text", StepUtils.getParameter(data, info,"test-info-non-exist", ""));
+  }
+
+  private PackageData createPackageData(Map<String, String> requestParameters) {
+    try {
+      Path tempFile = Files.createTempFile("test", "unit");
+      PackageData data = PackageData.newPackageData("fake", tempFile.toFile());
+      if (requestParameters != null) {
+        for (Map.Entry<String, String> parameter : requestParameters.entrySet()) {
+          data.setParameter(parameter.getKey(), parameter.getValue());
+        }
+      }
+      return data;
+    } catch (IOException io) {
+      Assert.fail(io.getMessage());
+    }
+    return null;
+  }
+
+  private StepInfo createStepInfo (Map<String, String> stepParameters) {
+    return new StepInfoImpl("id", "name", "input", "output", stepParameters);
+  }
+}
